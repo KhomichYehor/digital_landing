de84b6a614e58e984059135f192ce755d7aba70a
Yehor <eee.houmich@gmail.com>
2024-04-15 14:41:13 +0300

    Add responsive header and structure for slider

---
+++ src/index.js
@@ -0,0 +1,63 @@
+function checkWindowSize() {
+  if (window.innerWidth > 480) {
+    document.getElementById('navbar').style.display = 'flex';
+    document.body.style.overflow = 'auto';
+  } else {
+    document.getElementById('navbar').style.display = 'none';
+    document.body.style.overflow = 'auto';
+  }
+}
+
+document.addEventListener('DOMContentLoaded', checkWindowSize);
+
+window.addEventListener('resize', checkWindowSize);
+
+document.getElementById('menu-toggle').addEventListener('click', function () {
+  document.getElementById('navbar').style.display = 'flex';
+  document.body.style.overflow = 'hidden';
+});
+
+document.getElementById('close-menu').addEventListener('click', function () {
+  document.getElementById('navbar').style.display = 'none';
+  document.body.style.overflow = 'auto';
+});
+
+
+//Slider
+
+document.addEventListener('DOMContentLoaded', function() {
+  const imagesContainer = document.querySelector('.slider__images');
+  const slides = imagesContainer.children;
+  const totalSlides = slides.length;
+  const currentSlideElement = document.getElementById('current-slide');
+  const totalSlidesElement = document.getElementById('total-slides');
+  let currentSlideIndex = 0;
+
+  totalSlidesElement.textContent = totalSlides;
+  currentSlideElement.textContent = currentSlideIndex + 1;
+
+  function updateSlide(index) {
+      Array.from(slides).forEach((slide, idx) => {
+          slide.style.display = idx === index ? 'block' : 'none';
+      });
+      currentSlideElement.textContent = index + 1;
+  }
+
+  document.getElementById('prev-slide').addEventListener('click', function() {
+      currentSlideIndex = (currentSlideIndex - 1 + totalSlides) % totalSlides;
+      updateSlide(currentSlideIndex);
+  });
+
+  document.getElementById('next-slide').addEventListener('click', function() {
+      currentSlideIndex = (currentSlideIndex + 1) % totalSlides;
+      updateSlide(currentSlideIndex);
+  });
+
+  Array.from(document.querySelectorAll('.slider__item__btn')).forEach(button => {
+      button.addEventListener('click', function() {
+          const detailsDiv = button.nextElementSibling;
+          const isVisible = detailsDiv.style.display === 'block';
+          detailsDiv.style.display = isVisible ? 'none' : 'block';
+      });
+  });
+});
